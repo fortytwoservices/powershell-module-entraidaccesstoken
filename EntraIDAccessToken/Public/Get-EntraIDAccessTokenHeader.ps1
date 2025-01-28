6689cd0bc9be941972c3500731330f22501517c4
Marius Solbakken Mellum <marius.solbakken@fortytwo.io>
2025-01-28 09:45:49 +0100

    feat: Add AdditionalHeaders to Get-EntraIDAccessTokenHeader
    Signed-off-by: Marius Solbakken Mellum <marius.solbakken@fortytwo.io>

--- EntraIDAccessToken/Public/Get-EntraIDAccessTokenHeader.ps1
+++ EntraIDAccessToken/Public/Get-EntraIDAccessTokenHeader.ps1
@@ -14,12 +14,21 @@ function Get-EntraIDAccessTokenHeader {
         [String] $Profile = "Default",
 
         [Parameter(Mandatory = $false)]
-        [Switch] $ConsistencyLevelEventual
+        [Switch] $ConsistencyLevelEventual,
+
+        [Parameter(Mandatory = $false)]
+        [System.Collections.Hashtable] $AdditionalHeaders = $null
     )
 
     Process {
         $headers = @{
             Authorization = "Bearer $(Get-EntraIDAccessToken -Profile $Profile)"
+        }
+
+        if($AdditionalHeaders) {
+            $AdditionalHeaders.GetEnumerator() | ForEach-Object {
+                $headers[$_.Key] = $_.Value
+            }
         }
 
         if ($ConsistencyLevelEventual.IsPresent) {
