392a404ad562bd3955289221d22aa10820b7313e
Marius Solbakken Mellum <marius.solbakken@fortytwo.io>
2025-06-03 13:13:58 +0200

    fix: Add verification of number of dots to Get-EntraIDAccessTokenPayload
    Signed-off-by: Marius Solbakken Mellum <marius.solbakken@fortytwo.io>

--- EntraIDAccessToken/Public/Get-EntraIDAccessTokenPayload.ps1
+++ EntraIDAccessToken/Public/Get-EntraIDAccessTokenPayload.ps1
@@ -15,6 +15,11 @@ function Get-EntraIDAccessTokenPayload {
     )
 
     Process {
+        if($InputObject -notlike "*.*.*") {
+            Write-Error "InputObject is not a valid JWT token. Expected format: header.payload.signature"
+            return
+        }
+
         $payload = $InputObject.Split(".")[1]
         $payload = $payload.PadRight($payload.Length + (4 - ($payload.Length % 4)), "=").Replace("====", "")
         ConvertFrom-Json -InputObject ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($payload)))    
