370e9b10096652e8e7caa2f4a17de9b56c1542b8
Marius Solbakken Mellum <marius.solbakken@fortytwo.io>
2025-09-19 09:57:49 +0200

    feat: Add more tests for Add-EntraIDClientSecretAccessTokenProfile
    Signed-off-by: Marius Solbakken Mellum <marius.solbakken@fortytwo.io>

--- EntraIDAccessToken/Public/Add-EntraIDClientSecretAccessTokenProfile.Tests.ps1
+++ EntraIDAccessToken/Public/Add-EntraIDClientSecretAccessTokenProfile.Tests.ps1
@@ -48,3 +48,49 @@ Describe "Add-EntraIDClientSecretAccessTokenProfile.2" {
         ($AT | ConvertFrom-EntraIDAccessToken).Payload.aud | Should -Be "cfa8b339-82a2-471a-a3c9-0fc0be7a4093"
     }
 }
+
+Describe "Add-EntraIDClientSecretAccessTokenProfile.3" {
+    BeforeAll {
+        $Name = (New-Guid).ToString()
+        Add-EntraIDClientSecretAccessTokenProfile -Name $Name -ClientId $ENV:EIDATPESTERCLIENTID -ClientSecret (ConvertTo-SecureString $ENV:EIDATPESTERCLIENTSECRET -AsPlainText -Force) -TenantId $ENV:EIDATPESTERTENANTID -Scope "https://api.fortytwo.io/.default"
+    }
+
+    It "Creates a profile with client secret authentication" {
+        $P = Get-EntraIDAccessTokenProfile -Profile $Name 
+        $P.Name | Should -Be $Name
+        $P.AuthenticationMethod | Should -Be "clientsecret"
+        $P.ClientId | Should -Be $ENV:EIDATPESTERCLIENTID
+        $P.TenantId | Should -Be $ENV:EIDATPESTERTENANTID
+        $P.Scope | Should -Be "https://api.fortytwo.io/.default"
+    }
+
+    It "Returns an access token for the correct audience" {
+        $AT = Get-EntraIDAccessToken -Profile $Name
+        $AT | Should -BeLike "ey*.ey*.*"
+        ($AT | ConvertFrom-EntraIDAccessToken).Payload.aud | Should -Be "https://api.fortytwo.io"
+        ($AT | ConvertFrom-EntraIDAccessToken).Payload.ver | Should -Be "1.0"
+    }
+}
+
+Describe "Add-EntraIDClientSecretAccessTokenProfile.4" {
+    BeforeAll {
+        $Name = (New-Guid).ToString()
+        Add-EntraIDClientSecretAccessTokenProfile -Name $Name -ClientId $ENV:EIDATPESTERCLIENTID -ClientSecret (ConvertTo-SecureString $ENV:EIDATPESTERCLIENTSECRET -AsPlainText -Force) -TenantId $ENV:EIDATPESTERTENANTID -Scope "https://api.fortytwo.io/.default" -V2Token
+    }
+
+    It "Creates a profile with client secret authentication" {
+        $P = Get-EntraIDAccessTokenProfile -Profile $Name 
+        $P.Name | Should -Be $Name
+        $P.AuthenticationMethod | Should -Be "clientsecret"
+        $P.ClientId | Should -Be $ENV:EIDATPESTERCLIENTID
+        $P.TenantId | Should -Be $ENV:EIDATPESTERTENANTID
+        $P.Scope | Should -Be "https://api.fortytwo.io/.default"
+    }
+
+    It "Returns an access token for the correct audience" {
+        $AT = Get-EntraIDAccessToken -Profile $Name
+        $AT | Should -BeLike "ey*.ey*.*"
+        ($AT | ConvertFrom-EntraIDAccessToken).Payload.aud | Should -Be "https://api.fortytwo.io"
+        ($AT | ConvertFrom-EntraIDAccessToken).Payload.ver | Should -Be "2.0"
+    }
+}
