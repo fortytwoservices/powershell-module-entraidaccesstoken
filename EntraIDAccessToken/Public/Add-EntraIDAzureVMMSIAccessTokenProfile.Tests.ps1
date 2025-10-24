fae53eca1d16d6b7e880cf1425c6684c02152ed1
Marius Solbakken Mellum <marius.solbakken@fortytwo.io>
2025-10-24 15:49:31 +0200

    feat: Add test for Add-EntraIDAzureVMMSIAccessTokenProfile
    Signed-off-by: Marius Solbakken Mellum <marius.solbakken@fortytwo.io>

--- EntraIDAccessToken/Public/Add-EntraIDAzureVMMSIAccessTokenProfile.Tests.ps1
+++ EntraIDAccessToken/Public/Add-EntraIDAzureVMMSIAccessTokenProfile.Tests.ps1
@@ -1,5 +1,5 @@
 BeforeAll {
-    Import-Module ./EntraIDAccessToken -Force
+    $Script:Module = Import-Module ./EntraIDAccessToken -Force -PassThru
 }
 
 Describe "Add-EntraIDAzureVMMSIAccessTokenProfile.1" {
@@ -7,5 +7,24 @@ Describe "Add-EntraIDAzureVMMSIAccessTokenProfile.1" {
 
     It "Throws error when trying to use scope without a trusting application" {
         { Add-EntraIDAzureVMMSIAccessTokenProfile -Scope "https://graph.microsoft.com/.default" -TenantId "12345678-1234-1234-1234-123456789012" } | Should -Throw
+    }
+}
+
+Describe "Add-EntraIDAzureVMMSIAccessTokenProfile.2" {
+    BeforeAll {
+        Mock -ModuleName $Script:Module.Name -CommandName Invoke-WebRequest -MockWith {
+            return (@{
+                access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"
+                expires_in   = "3599"
+                expires_on   = (Get-Date -Date (Get-Date).AddHours(1) -UFormat %s)
+            }
+            |ConvertTo-Json)
+        }
+    }
+
+    It "Should return a mock token" {
+        Add-EntraIDAzureVMMSIAccessTokenProfile
+
+        Get-EntraIDAccessToken | Should -Be "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.KMUFsIDTnFmyG3nMiGM6H9FNFUROf3wh7SmqJp-QV30"
     }
 }
