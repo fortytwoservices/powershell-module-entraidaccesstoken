2575ba06a33d812e60425b9b525a4eb8d7235c94
Marius Solbakken Mellum <marius.solbakken@fortytwo.io>
2025-10-27 09:43:20 +0100

    fix: Add missing support for user assigned identity
    Signed-off-by: Marius Solbakken Mellum <marius.solbakken@fortytwo.io>

--- EntraIDAccessToken/Private/Get-EntraIDAzureVMMSIAccessToken.ps1
+++ EntraIDAccessToken/Private/Get-EntraIDAzureVMMSIAccessToken.ps1
@@ -13,8 +13,14 @@ function Get-EntraIDAzureVMMSIAccessToken {
         # TODO: Understand why documentation is pointing to http://localhost:50342/oauth2/token - https://learn.microsoft.com/en-us/entra/identity/managed-identities-azure-resources/how-to-use-vm-sign-in
         $uri = "http://169.254.169.254/metadata/identity/oauth2/token"
 
+
+        $clientid_param = ""
+        if ($AccessTokenProfile.UserAssignedIdentityClientId) {
+            $clientid_param = "&client_id=$($AccessTokenProfile.UserAssignedIdentityClientId)"
+        }
+
         $_resource = [String]::IsNullOrEmpty($Resource) ? $AccessTokenProfile.Resource : $Resource
-        $response = Invoke-WebRequest -Uri "$($uri)?resource=$([System.Uri]::EscapeDataString($_resource))&api-version=2018-02-01" -Headers @{Metadata = "true" } -UseBasicParsing
+        $response = Invoke-WebRequest -Uri "$($uri)?resource=$([System.Uri]::EscapeDataString($_resource))$($clientid_param)&api-version=2018-02-01" -Headers @{Metadata = "true" } -UseBasicParsing
 
         if ($response.StatusCode -ne 200) {
             throw "Error getting access token from Azure VM MSI endpoint: $($response.StatusCode) $($response.StatusDescription) $($response.Content)"
