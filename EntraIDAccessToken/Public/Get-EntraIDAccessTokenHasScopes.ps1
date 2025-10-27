7fbe98f292554d8477ff1e6eb1bdd8c195d9c51b
Marius Solbakken Mellum <marius.solbakken@fortytwo.io>
2025-10-27 15:33:44 +0100

    feat: Add Get-EntraIDAccessTokenHasScopes
    Signed-off-by: Marius Solbakken Mellum <marius.solbakken@fortytwo.io>

---
+++ EntraIDAccessToken/Public/Get-EntraIDAccessTokenHasScopes.ps1
@@ -0,0 +1,39 @@
+<#
+.SYNOPSIS
+Get a boolean indicating whether the input access token has all or any of the specified roles.
+
+.DESCRIPTION
+Get a boolean indicating whether the input access token has all or any of the specified roles.
+
+.EXAMPLE
+    PS> Get-EntraIDAccessToken |Get-EntraIDAccessTokenHasScopes -Scopes "Group.Read.All"
+
+.EXAMPLE
+    PS> Get-EntraIDAccessToken |Get-EntraIDAccessTokenHasScopes -Scopes "Group.Read.All", "Group.ReadWrite.All" -Any
+#>
+function Get-EntraIDAccessTokenHasScopes {
+    [CmdletBinding(DefaultParameterSetName = "All")]
+
+    Param(
+        [Parameter(Mandatory = $true)]
+        [String[]] $Scopes,
+
+        [Parameter(Mandatory = $true, ParameterSetName = "Any")]
+        [Switch] $Any,
+
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true)]
+        [String] $AccessToken
+    )
+
+    Process {
+        $Payload = $AccessToken | Get-EntraIDAccessTokenPayload
+
+        $PayloadScopes = $Payload.scp -split " " | ForEach-Object { $_.Trim() }
+
+        if($Any.IsPresent) {
+            return !!($Scopes | Where-Object {$PayloadScopes -contains $_})
+        }
+
+        return !($Scopes | Where-Object {$PayloadScopes -notcontains $_})
+    }
+}
