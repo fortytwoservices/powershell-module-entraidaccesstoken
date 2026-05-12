c8070628e29cf125f5720b655475814f829d1dbb
Marius Solbakken Mellum <marius.solbakken@fortytwo.io>
2026-05-12 14:54:09 +0200

    feat: Add cmdlet Get-EntraIDAdminConsentUrl

---
+++ EntraIDAccessToken/Public/Get-EntraIDAdminConsentUrl.ps1
@@ -0,0 +1,25 @@
+﻿<#
+.SYNOPSIS
+Generates an admin consent URL for a given application
+
+.DESCRIPTION
+Generates an admin consent URL for a given application
+
+.EXAMPLE
+Get-EntraIDAccessTokenProfile | Get-EntraIDAdminConsentUrl
+
+.EXAMPLE
+Get-EntraIDAdminConsentUrl -ClientId "your-client-id"
+#>
+function Get-EntraIDAdminConsentUrl {
+    [CmdletBinding()]
+
+    Param(
+        [Parameter(Mandatory = $true, ValueFromPipelineByPropertyName = $true)]
+        [String] $ClientId
+    )
+
+    Process {
+        return "https://login.microsoftonline.com/common/adminconsent?client_id=$ClientId"
+    }
+}
